871d51e8d2d5d6a4217e54e813119def6c33d0d4
Arthur MEHMETOGLU <amehmeto@tudent.42.fr>
2017-04-27 05:07:53 +0200

    1st try

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: amehmeto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/14 07:26:37 by amehmeto          #+#    #+#             */
-/*   Updated: 2017/04/26 18:54:57 by amehmeto         ###   ########.fr       */
+/*   Updated: 2017/04/27 03:35:15 by amehmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "Libft/libft.h"
+# include "srcs/libft.h"
 # include <fcntl.h>
 # include <stdio.h>
-# define BUFF_SIZE 14
+# define BUFF_SIZE 10
 
 int		get_next_line(const int fd, char **line);
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: amehmeto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/20 19:49:00 by amehmeto          #+#    #+#             */
-/*   Updated: 2017/04/26 21:45:03 by amehmeto         ###   ########.fr       */
+/*   Updated: 2017/04/27 03:55:38 by amehmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,27 @@
 
 int		main(int ac, char **av)
 {
-	int		fd;
+	int		fd, a;
 	char	*line; 
 
-	if (ac != 2)
+/*	if (ac != 2)
 	{
 		ft_putstr("usage: ./gnl source_file\n");
 		return (0);
-	}
+	}*/
+	(void)ac;
+	(void)av;
 	if ((fd = open(av[1], O_RDONLY)) == -1)
 		ft_putstr("open error\n");
 
+	a = 1;
 	line = NULL;
-	get_next_line(fd, &line);
-	//printf("n# of char in line = %zu\n", ft_strlen(line));
-	printf("%s\n", line);
-	get_next_line(fd, &line);
-	//printf("n# of char in line = %zu\n", ft_strlen(line));
-	printf("%s", line);
+	while (a > 0)
+	{
+		//a = get_next_line(fd, &line);
+		a = get_next_line(fd, &line);
+		printf("%s\t  a = %d\n", line, a);
+	}
 
 	if (close(fd) == -1)
 		ft_putstr("close error\n");
